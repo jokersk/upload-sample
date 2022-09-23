1b19518d1c41187f74b30f5e4ab34236c4e15efc
jokersk <jokersk119@gmail.com>
2022-09-23 17:45:26 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,32 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { FileUploader } from "react-drag-drop-files";
-import axios from 'axios'
+import { useForm } from "react-hook-form";
+import axios from "axios";
 import "./App.css";
 function App() {
-  const [name, setName] = useState('');
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
   const [file, setFile] = useState(null);
   const handleChange = (file) => {
     setFile(file);
   };
-  const submit = (e: Event) => {
-      e.preventDefault()
-      const data = new FormData()
-      data.append('name', name)
-      data.append('file', file)
-      axios.post('some url', data)
-  }
+  const onSubmit = (state) => {
+    const data = new FormData();
+    data.append("name", state.name);
+    data.append("file", file);
+    axios.post("some url", data);
+  };
   return (
     <div className="App">
-      <form action="" onSubmit={submit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="name">name</label>
-        <input type="" name="name" id="name" value={ name } onInput={e => setName(e.target.value)} />
-        <FileUploader
-          handleChange={handleChange}
-          name="file"
-        />
+        <input {...register("name")} />
+        <FileUploader handleChange={handleChange} name="file" />
         <button type="submit">submit</button>
       </form>
     </div>
