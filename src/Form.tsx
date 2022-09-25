a2f4b116d0d128894e7068982ef21b71c8708435
jokersk <jokersk119@gmail.com>
2022-09-25 23:19:21 +0800

    wip

---
+++ src/Form.tsx
@@ -0,0 +1,14 @@
+function Form() {
+    return <form action="" className="grid grid-cols-[auto,1fr] gap-2 max-w-lg items-center">
+        <label htmlFor="name">name</label>
+        <input type="" id="name" className="border px-4 py-2" />
+        <label htmlFor="parentId">Parent Id *</label>
+        <input type="" id="parentId" className="border px-4 py-2" />
+        <label htmlFor="long">I am super longggggggggggg</label>
+        <input type="" id="long" className="border px-4 py-2" />
+        <label htmlFor="parentId">Parent Id *</label>
+        <input type="" id="parentId" className="border px-4 py-2" />
+    </form>
+}
+
+export default Form;
