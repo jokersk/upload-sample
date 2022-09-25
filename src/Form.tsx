dc91638d58174a36ade66e5a9f5afcf81b835919
jokersk <jokersk119@gmail.com>
2022-09-25 23:29:57 +0800

    wip

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,14 +1,15 @@
 function Form() {
     return <form action="" className="grid grid-cols-[auto,1fr] gap-2 max-w-lg items-center">
-        <label htmlFor="name">name</label>
-        <div>
-            <input type="" id="name" className="border px-4 py-2" />
-        </div>
-        <label htmlFor="long">I am super longggggggggggg</label>
-        <div>
-            <input type="" id="long" className="border px-4 py-2" />
-        </div>
+        <Group label="name" />
+        <Group label="I am super longggggggggggg"/>
     </form>
+}
+
+function Group({ label }: { label: string }) {
+    return <>
+        <label htmlFor={label}>{ label }</label>
+        <input type="" id={label} className="border px-4 py-2" />
+    </>
 }
 
 export default Form;
