bf5fef3435562598723a4afae59b18cd9a1288fe
jokersk <jokersk119@gmail.com>
2022-09-25 23:34:34 +0800

    wip

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,14 +1,17 @@
 function Form() {
     return <form action="" className="grid grid-cols-[auto,1fr] gap-2 max-w-lg items-center">
         <Group label="name" />
-        <Group label="I am super longggggggggggg"/>
+        <Group label="I am super longggggggggggg" error="some error"/>
     </form>
 }
 
-function Group({ label }: { label: string }) {
+function Group({ label, error }: { label: string, error?: string }) {
     return <>
         <label htmlFor={label}>{ label }</label>
-        <input type="" id={label} className="border px-4 py-2" />
+        <div>
+            <input type="" id={label} className="border px-4 py-2" />
+        </div>
+        { error && <div className="col-start-2 text-red-500">{ error }</div> }
     </>
 }
 
